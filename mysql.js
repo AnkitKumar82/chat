ef045c7fdbb880e7254e40ee76558c08ff1bdc13
Ankit Kumar <kumarankit1041999@gmail.com>
2020-10-05 14:14:10 +0530

    Changes db credentials

--- mysql.js
+++ mysql.js
@@ -11,21 +11,18 @@ Server: remotemysql.com
 Port: 3306*/
 
 //Local host data base info
+// var con = mysql.createConnection({
 // host: "localhost",
 //   user: "root",
 //   password: "Ankit@1234",
 //   database: "ankitpersonal"
 // });
-/*
-sql12.freemysqlhosting.net
-name and databaes:sql12366720 	sql12366720
-password: aYxW7ruxTV
-*/
+
 var con = mysql.createConnection({
   host: "sql12.freemysqlhosting.net",
-  user: "sql12366720",
-  password: "aYxW7ruxTV",
-  database: "sql12366720"
+  user: "sql12368881",
+  password: "7k7pvKAjzs",
+  database: "sql12368881"
 });
 
 con.connect(function(err) {
